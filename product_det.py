127f73974abcff060056b170f4ba2e4dbee81586
Rutu <kinnalrutu@gmail.com>
2025-12-14 20:06:28 +0530

    Create product_det.py

---
+++ product_det.py
@@ -0,0 +1,13 @@
+import sys
+
+def get_product_info(product_name, product_id, quantity, price):
+  
+    return (
+        f"product Name:{product_name},"
+        f"product ID:{emp_id},"
+        f"Quantity:{quantity},"
+        f"price:{price:.2f}"
+    )
+if __name__ == "__main__":
+    print("=== product details ===")
+    print(get_employee_info("smart watch","123","4",550))
